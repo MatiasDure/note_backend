c223bf911e0b590cf3c7beeebe5805d3a4cabd11
Matias Dure <91970565+MatiasDure@users.noreply.github.com>
2024-01-08 22:06:55 +0100

    Added custom middlewares

--- middlewares.js
+++ middlewares.js
@@ -1,1 +1,24 @@
+const requestLogger = (req, res, next) => {
+	const origin = `${req.protocol}://${req.hostname}${req.path}`;
+	console.log(`\n-----${req.method} request-----\n-----Origin: ${origin}-----\n-----Body: ${JSON.stringify(req.body)}-----\n`);
+	next();
+};
 
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: "Unknown endpoint" });
+};
+
+const errorHandler = (err, req, res, next) => {
+	console.log(err.message);
+
+	if(err.name === "CastError") return res.status(400).json({ error: "malformatted id" });
+	else if(err.name === "ValidationError") return res.status(400).json({ error: err.message });
+
+	next(err);
+};
+
+module.exports = {
+    requestLogger,
+    unknownEndpoint,
+    errorHandler
+};
